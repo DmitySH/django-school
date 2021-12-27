251d84460567743a363e8294818e19d2a6050539
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-27 20:49:36 +0300

    11 done

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -2,11 +2,42 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInline(admin.StackedInline):
+    model = Review
+    extra = 1
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    fields = (('actors', 'directors', 'genres'),)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'),)
+        })
+    )
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShot)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Review)
